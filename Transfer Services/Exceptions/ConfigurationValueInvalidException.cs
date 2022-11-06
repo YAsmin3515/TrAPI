5f345b488b997d4b74bc69c83a4247cef1c2ab3b
YAsmin3515 <117500206+YAsmin3515@users.noreply.github.com>
2022-11-06 00:17:20 -0400

    Add files via upload

---
+++ Transfer Services/Exceptions/ConfigurationValueInvalidException.cs
@@ -0,0 +1,22 @@
+﻿
+namespace landm.Transfer.Sample.Exceptions
+{
+    using System;
+
+    public class ConfigurationValueInvalidException: Exception
+    {
+        public ConfigurationValueInvalidException()
+        {
+        }
+
+        public ConfigurationValueInvalidException(string message)
+            : base(message)
+        {
+        }
+
+        public ConfigurationValueInvalidException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
